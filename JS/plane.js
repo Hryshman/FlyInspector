a0ea59c49f8d2eeb6fb80867bcb8d5f71e0eb34e
Hryshman <alexgryshman@gmail.com>
2017-03-21 17:11:36 +0200

    test

--- JS/plane.js
+++ JS/plane.js
@@ -1,138 +1,27 @@
-console.log('start');
+(function () {
+    'use strict';
 
-var sphere = {
-    id: 5,
-    name: 'This is sphere', // string
-    radius: 34, // number
-    isAlive: true, // boolean
-    sphereChild: { //obj
-        manyChildren: new Array("true", 1, 5), // array
-    },
-    gender: null, // nullable
-    color: "red",
-    size: 11
-};
-
-var planet = {
-    id: 4,
-    name: 'planet',
-    paws: {
-        name_of_planet: 'Mercury',
-        distance_from_the_Sun: 57.9,
-        obliquity: 7.005,
-        period_around_its_axis: 58.6
-    },
-    presence_of_satellite: false,
-    color: "green",
-    size: 200
-};
-
-var ship = {
-    id: 3,
-    name: 'ship',
-    sails: {
-        count: 2,
-        color: 'black'
-    },
-    equipment: {
-        firearms: ["missles", "guns", "muskets"],
-        armors: 'light'
-    },
-    color: "white",
-    size: 33
-};
-
-var ananas = {
-    id: 2,
-    name: 'pineapple',
-    color: {
-        colorHead: 'green'
-    },
-    color2: {
-        colorBody: 'yellow'
-    },
-    properties: {
-        skin: 'inedible',
-        insides: 'edible'
-    },
-    materials: {
-        aroundTheBody: 'skin',
-        head1: 'leaves',
-        heaobjd2: 'inedible'
-    },
-    edible: true,
-    fruit: true,
-    vegetable: false, //some people confused
-    quantity: 1,
-    skinThickness: 0.50,
-    color: "brown",
-    size: 400
-};
-
-var plane = {
-    id: 1,
-    name: 'Its plane',
-    characteristics: {
-        maxspeed: 900,
-        speed: [0, 900],
-        emptyWeight: 16.380,
-        maxWeight: 30.450
-    },
-    inAir: false,
-    onGround: true,
-    color: "silver",
-    size: 50
-}
-
-var polo = {
-    id: 6,
-    name: 'vw-polo',
-    data: {
-        width: 1560,
-        height: 1350,
-        weight: 690
-    },
-    engine: 'petrol',
-    qualitative: true,
-    color: "black",
-    size: 221
-};
-
-var fish = {
-    id: 7,
-    name: 'fish',
-    fishClass: 'Chondrichthyes',
-    fishLenght: 15,
-    friendly: false,
-    biologicalClassification: {
-        classification: ["Eukaryota", "Metazoa", "Eumetazoa", "Chordata", "Craniata", "Gnathostomata", "Chondrichthyes"],
-    },
-    legs: null,
-    family: undefined,
-    color: "blue",
-    size: 29
-};
-var items = [sphere, planet, ship, ananas, plane, polo, fish];
-    console.log(items.length);
-    y=items.length;
-var multi=1;
-debugger;
-var service = {
-     findMyObj: function (items, id, name) {
-          for (var x = 0; x < y; x++){
-          if(items[x].id === id && items[x].name === name ) {
-            return items[x];
-         }
-       }
-     },
-      calcMult: function (items) {
-           for (var x = 0; x < y; x++){
-             multi*=items[x].size;
-              }
-                return multi;
-        }
-      }
+    console.log('start');
 
 
-console.log('service', service.findMyObj(items, 1, 'Its plane'));
-console.log('service', service.calcMult(items));
+    var root = document.getElementById('global');/*запись в переменную дива, который создат в html теле*/
+    function next (){
+      alert("Как дела?")
+    }
+    root.addEventListener("contextmenu", next); /*событие на правый щелчек мышки*/
+
+    var linkg = document.getElementById('linkgit');
+    linkg.style.background = '#040503';
+    linkg.style.border = '1px solid white';
+
+    $('<div style="margin: 0 auto; text-align: center" id="menu">Привет</div>').insertAfter('#global').css('background','green').width(200);
+     $('<div style="margin: 0 auto; text-align: center" id="menu1">Привет</div>').insertAfter('#menu').css('background','red').width(200);
+      $('<div style="margin: 0 auto; text-align: center" id="menu2">Привет</div>').insertAfter('#menu1').css('background','blue').width(200);
+        $('<div style="margin: 0 auto; text-align: center" id="menu3">Привет</div>').insertAfter('#menu2').css('background','orange').width(200);
+        menu.style.border = '1px solid black';
+        menu1.style.border = '1px solid black';
+        menu2.style.border = '1px solid black';
+        menu3.style.border = '1px solid black';
+
+
+})();
