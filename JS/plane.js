fcb143bf8f506bcbd8b8d8ad6cea65c7fcef79e3
Hryshman <alexgryshman@gmail.com>
2017-03-27 17:02:26 +0300

    Lecture 3

--- JS/plane.js
+++ JS/plane.js
@@ -4,27 +4,38 @@
     console.log('start');
 
     function next (){
-      alert("Ссылка на гит репозиторий с данным сайтом?")
+      alert("Ссылка на гит репозиторий с данным сайтом")
     }
 
     var linkg = document.getElementById('linkgit');
-    linkg.style.background = '#040503';
+    linkg.style.background = '#1b2c3f';
     linkg.style.border = '1px solid white';
     linkg.addEventListener("contextmenu", next); /*событие на правый щелчек мышки*/
 
-    $('<div style="margin: 0 auto; text-align: center" id="menu"><a style="color: black" href="https://www.youtube.com/watch?v=ahOArT_hrZE">Buy in Aliexpress</a></div>').insertAfter('#global').css({'background':'red','fontFamily':'serif','border':'1px solid black'}).width(200);
-     $('<div style="margin: 0 auto; text-align: center" id="menu1">Привет</div>').insertAfter('#menu').css({'background':'grey', 'fontFamily':'inherit','border':'1px solid black'}).width(200);
-      $('<div style="margin: 0 auto; text-align: center" id="menu2">Привет</div>').insertAfter('#menu1').css({'background':'blue', 'fontFamily':'monospace','border':'1px solid black'}).width(200);
-        $('<div style="margin: 0 auto; text-align: center" id="menu3">Final (open redmine)</div>').insertAfter('#menu2').css({'background':'yellow', 'fontSize':'26px', 'fontFamily':'cursive','border':'1px solid black'}).width(200);
-    menu1.innerHTML = '<a id="color1" href="http://bootstrap-3.ru/getting-started.php">Get start with bootstrap</a>';
-    menu2.innerHTML = '<a id="color2" href="https://api.jquery.com/contextmenu/">Кликни правой кнопкой</a>';
-    menu3.innerHTML = '<a id="color3" href="http://redmine.luch15.com:8081/projects/students/issues?set_filter=1&tracker_id=3">Кликни правой кнопкой</a>';
-    document.getElementById('color1').style.color = 'red';
-    document.getElementById('color3').style.color = 'blue';
+    $('<div style="margin: 0 auto; text-align: center" id="menu"><a style="color: black" href="https://www.youtube.com/watch?v=UbQgXeY_zi4&list=PLhInz4M-OzRUsuBj8wF6383E7zm2dJfqZ">Music</a></div>').insertAfter('#global').css({'background':'#ffb701','fontFamily':'serif','border':'1px solid black'}).width(200);
+     $('<div style="margin: 0 auto; text-align: center" id="menu1"></div>').insertAfter('#menu').css({'background':'#ff9302', 'fontFamily':'inherit','border':'1px solid black'}).width(200);
+      $('<div style="margin: 0 auto; text-align: center" id="menu2"></div>').insertAfter('#menu1').css({'background':'#618b44', 'fontFamily':'monospace','border':'1px solid black'}).width(200);
+        $('<div style="margin: 0 auto; text-align: center" id="menu3"></div>').insertAfter('#menu2').css({'background':'#c7e1fd', 'fontSize':'26px', 'fontFamily':'cursive','border':'1px solid black'}).width(200);
+    menu1.innerHTML = '<a id="color1" href="http://bootstrap-3.ru/getting-started.php">Кликни правой кнопкой на этот background</a>';
+    menu2.innerHTML = '<a id="color2" href="https://api.jquery.com/contextmenu/">Doubleclick on this background</a>';
+    menu3.innerHTML = '<a id="color3" href="http://redmine.luch15.com:8081/projects/students/issues?set_filter=1&tracker_id=3">Redmine (Click on this background)</a>';
+    color1.style.color = '#3d5a68';
+    color3.style.color = 'blue';
 
-    /*menu1.style.color = prompt('color?', 'green');*/
-   $('#color2').contextmenu(function(){
-     document.getElementById('color2').style.color = prompt('Напиши цвет : red, white, orange, yellow?', '')
-   });
+    $('#menu1').contextmenu(function(){
+      document.getElementById('color1').style.color = prompt('Напиши цвет : red, white, orange или yellow?', '');
+    });
+
+    menu2.ondblclick = function () {
+       color2.style.color = '#ff9302';
+       menu3.style.background = '#c7e1fd';
+     };
+
+   $('#menu3').click (function() {
+        $(this).css('background', '#67a9d2');
+        $('#color2').html('<a href="https://api.jquery.com/contextmenu/">Doubleclick once more on this background</a>').css('color', 'white');
+    });
+   var n = $('#counter').html;
+   console.log(n);
 
 })();
